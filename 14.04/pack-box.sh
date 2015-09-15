16d8107ee7bac550f54623e891d34c8bc45ea3ed
t-iwano <t-iwano@axsh.net>
2015-09-15 16:00:26 +0900

    Added default version and size.

--- 14.04/pack-box.sh
+++ 14.04/pack-box.sh
@@ -5,6 +5,11 @@
 #
 set -e
 
+declare version=$1
+declare file_size=$2
+
+version=${version:-3}
+file_size=${file_size:-30}
 qcow2_file=$(ls ubuntu-kvm | grep .qcow2)
-time qemu-img convert -O raw ubuntu-kvm/${qcow2_file} ubuntu-14.04-30g.raw
-time tar zScvf ubuntu-14.04-30g.kvm.box ubuntu-14.04-30g.raw
+time qemu-img convert -O raw ubuntu-kvm/${qcow2_file} ubuntu-14.04.${version}-${file_size}g.raw
+time tar zScvf ubuntu-14.04.${version}-${file_size}g.kvm.box ubuntu-14.04.${version}-${file_size}g.raw
